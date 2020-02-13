3a765ac3c35ac4ccd89cc4c3d6acc3f93d4bedc2
Vanessa Martinez <vmar76@gmail.com>
2020-02-13 21:12:40 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,22 +1,47 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
+require 'pry' 
+
+def pretty_print_nds(nds)
+  pp nds 
+  nil
+end 
+pretty_print_nds(directors_database)
 
 def directors_totals(nds)
-  # Remember, it's always OK to pretty print what you get *in* to make sure
-  # that you know what you're starting with!
-  #
-  #
-  # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
-  result = {
-  }
-  #
-  # Use loops, variables and the accessing method, [], to loop through the NDS
-  # and total up all the
+
+  hash = {}
+  
+  # iterate and add totals
   # ...
-  # ...
-  # ...
-  #
-  #
-  # Be sure to return the result at the end!
-  nil
-end
+  # return the hash
+  
+row_index = 0 
+while row_index < nds.length do 
+  name = nds[row_index][:name]
+  hash[name] = 0
+  movies = nds[row_index][:movies]
+  counter = 0 
+  while counter < movies.length do
+  binding.pry 
+      end
+      counter += 1 
+  row_index += 1 
+  p grand_total
+end 
+end 
+
+  # result = {
+#   }
+  
+#   #
+#   # Use loops, variables and the accessing method, [], to loop through the NDS
+#   # and total up all the
+#   # ...
+#   # ...
+#   # ...
+#   #
+#   #
+#   # Be sure to return the result at the end!
+#   nil
+# end
